aafaebc29013edeaa0ffe90b88f281fad42eb924
Rafaela Brito <rcfbritto@gmail.com>
2023-11-09 17:21:31 -0300

    Aula 1 dotNet

---
+++ Semana1/MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region Teste de Tipos de Dados
+    int tipoInteiro;
+    double tipoDouble;
+    string tipoString;
+    long tipoLong;
+
+    tipoInteiro = int.MaxValue;
+    tipoLong = long.MaxValue;
+
+    tipoString = "100A";
+    tipoInteiro = int.TryParse(tipoString);
+
+    System.Console.WriteLine("O máximo inteiro é " +tipoInteiro);
+    System.Console.WriteLine("O máximo long é " +tipoLong);
+
+#endregion
+
+#region Operadores
+    tipoDouble = tipoInteiro + tipoLong;
+
+
+    tipoInteiro = 10 > 5 ? 1 : 0;
+#endregion
