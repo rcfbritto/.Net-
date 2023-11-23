c478babd873b8fcf94cc5074d7a1451aa2ddb65a
Rafaela Brito <rcfbritto@gmail.com>
2023-11-23 17:20:21 -0300

    Exemplos aula

---
+++ Semana3/exemplosAula/Program.cs
@@ -0,0 +1,18 @@
+﻿
+List<(string, int)> pessoasAlturas = new List<(string, int)>
+{
+    ("João", 175),
+    ("Maria", 162),
+    ("Brito", 183),
+    ("Rafaela", 165),
+    ("Cristina", 157)
+};
+
+// Expressão lambda para extrair a altura de cada pessoa
+Func<(string, int), int> altura = pessoa => pessoa.Item2;
+
+// Calculando a média das alturas usando LINQ
+double mediaAlturas = pessoasAlturas.Select(altura).Average();
+
+Console.WriteLine($"Altura média das pessoas: {mediaAlturas} cm");
+
