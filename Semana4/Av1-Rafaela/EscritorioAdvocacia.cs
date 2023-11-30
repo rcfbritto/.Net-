4e024c815e41c77efbe5e0f0bbd4024736a402cd
Rafaela Brito <rcfbritto@gmail.com>
2023-11-30 16:17:00 -0300

    Editando classes

--- Semana4/Av1-Rafaela/EscritorioAdvocacia.cs
+++ Semana4/Av1-Rafaela/EscritorioAdvocacia.cs
@@ -1,27 +1,38 @@
 namespace Namespace;
 public class EscritorioAdvocacia
 {
-    private List<Advogado> advogados;
-    private List<Cliente> clientes;
-    
-    advogados = new List<Advogado>();
-    clientes = new List<Cliente>();
-    
-    public EscritorioAdvocacia()
-    {
-        advogados = new List<Advogado>();
-        clientes = new List<Cliente>();
-    }
+        protected List<Advogado> advogados = new List<Advogado>();
+        protected List<Cliente> clientes = new List<Cliente>();
+
     public void AdicionarAdvogado(Advogado advogado){
-        if((advogados.Exists(a => a.CPF == advogado.CPF) || advogados.Exists(a => a.CNA == advogado.CNA)))
+        if(advogados.Exists(x => x.CPF == advogado.CPF|| x.CNA == advogado.CNA))  
         {
             System.Console.WriteLine("Advogado com CPF ou CNA já cadastrado!");
         }
         else
         {
-            advogados.Add();
+            advogados.Add(advogado);
             System.Console.WriteLine("Advogado adicionado com sucesso!");
+        }      
+    }
+    public void AdicionarCliente(Cliente cliente){
+        if(clientes.Exists(x => x.CPF == cliente.CPF))
+        {
+            System.Console.WriteLine("Cliente com CPF já cadastrado!");
         }
-            
+        else{
+            clientes.Add(cliente);
+        }
+    }
+    public void RemoverAdvogado(Advogado advogado){
+        if(!advogados.Exists(x => x.CPF == advogado.CPF || x.CNA == advogado.CNA)){
+            System.Console.WriteLine("Advogado não encontrado!");
+        }
+        else{
+             advogados.Remove(advogado);
+        }
+    }
+    public void RemoverCliente(Cliente cliente){
+        
     }
 }
